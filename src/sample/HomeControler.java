be5405b80d259ebc4763ddeb173eca202aa3c77e
Martin Mochnacky <mochnacky@spse-po.sk>
2019-05-10 10:14:57 +0200

    Vsetci su radi lebo to ide

--- src/sample/HomeControler.java
+++ src/sample/HomeControler.java
@@ -32,20 +32,25 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class HomeControler implements Initializable {
+
+    private ResultSet date, resultSetSize;
     @FXML
     private Label cas;
     @FXML
     private Label den;
-@FXML
-private ImageView icon;
+    @FXML
+    private ImageView icon;
     @FXML
     private Label teplota;
-
     @FXML
-    private Label balance,balanceText;
-
+    private Label nameDay;
+    @FXML
+    private Label nameDayTomorrow;
     @FXML
     private Line balanceLine;
+
+    @FXML
+    private Label balance, balanceText;
 
     private String money;
     private PreparedStatement statement = null;
@@ -53,18 +58,18 @@ private ImageView icon;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        if(!loginController.curentlyLoggedUser.getType().equals("admin")){
+        if (!loginController.curentlyLoggedUser.getType().equals("admin")) {
             balance.setVisible(false);
             balanceText.setVisible(false);
             balanceLine.setVisible(false);
         }
 
+
         Connection connection = ConnectionClass.getConnection();
         System.out.println("CONNECTION INICIATED");
         String sql = "SELECT stav FROM bankovy_ucet WHERE id IS 1";
 
-        try{
+        try {
             statement = connection.prepareStatement(sql);
             ResultSet setOfData = statement.executeQuery();
 
@@ -72,7 +77,7 @@ private ImageView icon;
             //acc.setId(setOfData.getInt("id"));
             acc.setStav(setOfData.getDouble("stav"));
 
-        }catch (SQLException ex) {
+        } catch (SQLException ex) {
             Logger.getLogger(RequestUsersData.class.getName()).log(Level.SEVERE, null, ex);
         } finally {
             if (connection != null) {
@@ -97,24 +102,29 @@ private ImageView icon;
 
         balance.setText(money + "€");
         initClock();
+        try {
+            odosli();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
-
-        Weather pocasie =new Weather();
-        teplota.setText(Integer.toString(pocasie.getTemp())+ " °C");
+        Weather pocasie = new Weather();
+        teplota.setText(Integer.toString(pocasie.getTemp()) + " °C");
         System.out.println(pocasie.getDesc());
-        Image img1 =new Image("image/pocasie1.png");
-        Image img2 =new Image("image/pocasie2.png");
-        Image img3 =new Image("image/pocasie3.png");
-        Image img4 =new Image("image/pocasie4.png");
-        Image img5 =new Image("image/pocasie5.png");
-        Image img6 =new Image("image/pocasie6.png");
-        Image img7 =new Image("image/pocasie7.png");
-        Image img8 =new Image("image/pocasie8.png");
-        Image img9 =new Image("image/pocasie9.png");
-        Image img10 =new Image("image/load.gif");
+        Image img1 = new Image("image/pocasie1.png");
+        Image img2 = new Image("image/pocasie2.png");
+        Image img3 = new Image("image/pocasie3.png");
+        Image img4 = new Image("image/pocasie4.png");
+        Image img5 = new Image("image/pocasie5.png");
+        Image img6 = new Image("image/pocasie6.png");
+        Image img7 = new Image("image/pocasie7.png");
+        Image img8 = new Image("image/pocasie8.png");
+        Image img9 = new Image("image/pocasie9.png");
+        Image img11 = new Image("image/pocasie10.png");
+        Image img10 = new Image("image/load.gif");
 
-        switch(pocasie.getDesc()) {
+        switch (pocasie.getDesc()) {
             case "clear sky":
                 icon.setImage(img1);
                 break;
@@ -142,6 +152,9 @@ private ImageView icon;
             case "mist":
                 icon.setImage(img9);
                 break;
+            case "light rain":
+                icon.setImage(img11);
+                break;
             default:
                 icon.setImage(img10);
         }
@@ -152,7 +165,7 @@ private ImageView icon;
         Calendar c = Calendar.getInstance();
         c.setTime(yourDate);
         int dayOfWeek = c.get(Calendar.DAY_OF_WEEK);
-        switch(dayOfWeek) {
+        switch (dayOfWeek) {
             case 1:
                 den.setText("nedeľa");
                 break;
@@ -189,36 +202,88 @@ private ImageView icon;
         clock.play();
     }
 
-/*
+
     private void odosli() throws SQLException {
-        String pattern = "dd.MM.yyyy";
+
+
+        String pattern = "d.M";
 
         DateFormat df = new SimpleDateFormat(pattern);
 
         Date today = Calendar.getInstance().getTime();
+        Date dt = new Date();
+        Calendar c = Calendar.getInstance();
+        c.setTime(dt);
+        c.add(Calendar.DATE, 1);
+        dt = c.getTime();
 
         String todayString = df.format(today);
+        String tomorrowString = df.format(dt);
 
 
-        System.out.println(todayString);
-
-        String xLogin="ajoj";
-
-        String sqlEmail = "SELECT meno FROM zviera WHERE datum_narodenia = ?" ;
+        String menoToday = "";
+        String menoTomorrow = "";
 
 
         Connection connection = ConnectionClass.getConnection();
+        String sqlQuery = "SELECT * FROM zviera";
+        String countQuery = "SELECT Count(*) FROM zviera";
 
-        PreparedStatement statementForEmail = connection.prepareStatement(sqlEmail);
-        statementForEmail.setString(1, xLogin);
-        ResultSet Meno = statementForEmail.executeQuery();
-        Meno.next();
-
-        if (!Meno.isClosed() || !Meno.isClosed()) {
-
-
-            connection.close();
+        PreparedStatement preparedQuery, preparedCountStatement;
+        try {
+            preparedCountStatement = connection.prepareStatement(countQuery);
+            resultSetSize = preparedCountStatement.executeQuery();
+            resultSetSize.next();
+            preparedQuery = connection.prepareStatement(sqlQuery);
+            date = preparedQuery.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-    }*/
+
+
+        int dlzka = Integer.parseInt(resultSetSize.getString(1));
+
+        date.next();
+        for (int i = 0; i < dlzka; i++) {
+            if (!date.isClosed()) {
+                String s = "";
+                int p = 0;
+                for (int j = 0; j < date.getString(3).length(); j++) {
+                    if (date.getString(3).charAt(j) == '.') {
+                        p++;
+                    }
+
+                    if (p < 2) {
+                        s = s + date.getString(3).charAt(j);
+                    }
+
+                }
+
+
+                if (s.equals(todayString)) {
+                    menoToday = menoToday + "," + date.getString(2);
+                }
+                if (s.equals(tomorrowString)) {
+                    menoTomorrow = menoTomorrow + "," + date.getString(2);
+                }
+
+                date.next();
+
+            }
+        }
+        System.out.println(menoTomorrow);
+        try {
+            connection.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (menoToday.length() == 0) menoToday = " ------";
+        if (menoTomorrow.length() == 0) menoTomorrow = " ------";
+        nameDay.setText(menoToday.substring(1));
+        nameDayTomorrow.setText(menoTomorrow.substring(1));
+
 
     }
+}
